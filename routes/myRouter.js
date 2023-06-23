d34e757008903197ad6fd7a213420687e22a75b0
Pajan <d47708394@alumnos.itr.edu.ar>
2023-06-22 19:31:28 -0600

    pusheo BIEN hecho

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -1,7 +1,28 @@
-const myController = require('../controllers/myController.js');
+const myController = require('../controllers/myController');
 const express = require('express');
 const router = express.Router();
+import {getUserDetails} from '../services/userService.js';
 
-router.route('/juego/cartas').post(myController.crearDoc);
+router.route('/').get(myController.juego);
+
+const Jugador = require('../models/jugador');
+
+router.get('/jugadores', async (req, res, next) => {
+    try {
+        const jugadores = await Jugador.find();
+        res.json(jugadores);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post('/hello',async (req,res)=>{
+    let uname = req.body.nombre;
+    let userDetails = await getUserDetails(req.db,uname)
+    res.status(200).send({
+       status:true,
+       response:userDetails
+    });
+ });
 
 module.exports = router;
---
+++ services/userService.js
@@ -0,0 +1,17 @@
+//CAMBIAR EL "RUTA" POR EL NOMBRE DE LA COLECCION
+
+export const getUserDetails = (db, ruta) => {
+    return new Promise((resolve, reject) => {
+        db.collection('alumnos')
+            .find({ 'alumnos': ruta })
+            .toArray((err, docs) => {
+                if (err) {
+                    reject(err);
+                } else if (docs && docs.length > 0) {
+                    resolve(docs[0]);
+                } else {
+                    reject(new Error('No se encontro la ruta'));
+                }
+            });
+    });
+};
